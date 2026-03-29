0d1e3ed88d5389ac63ec2b68a0b9279f0f03a51b
gsantosPerson <gustavo.oliveira.person@gmail.com>
2026-03-29 00:44:55 -0300

    Primeiro commit do segundo exe

---
+++ exe2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+int vet[5];
+
+for(int i = 0; i < 5; i++){
+    scanf("%d", &vet[i]);
+}
+
+for(int i = 0; i < 5; i++){
+    printf("%d ", vet[i]);
+}
+return 0;
+}
